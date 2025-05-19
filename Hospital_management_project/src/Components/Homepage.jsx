287666e09d7e6d01700973a6601cb5de3af2bd08
vishalsolanki <vishalsolanki4880@gmail.com>
2025-05-19 10:07:56 +0530

    update

--- Hospital_management_project/src/Components/Homepage.jsx
+++ Hospital_management_project/src/Components/Homepage.jsx
@@ -1,27 +1,56 @@
+import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
 
+function Home() {
+    const [patients, setPatients] = useState([]);
+    const navigate = useNavigate();
 
-function Homepage() {
+    useEffect(() => {
+        const storedPatients = localStorage.getItem("hospitalPatients");
+        if (storedPatients) {
+            setPatients(JSON.parse(storedPatients));
+        }
+    }, []);
+
     return (
-        <>
-            <div className="container mt-4">
-                <h2 className='text-center mb-5'>Patient List</h2>
-                <Table striped bordered hover>
+        <div className="container mt-4">
+            <h2 className='mt-5 mb-5'>Registered Patients</h2>
+
+            {patients.length === 0 ? (
+                <p>No Date Found.</p>
+            ) : (
+                <Table bordered hover>
                     <thead>
                         <tr>
+                            <th>T.No</th>
                             <th>Patient Name</th>
-                            <th>Mobile Number</th>
+                            <th>Mobile</th>
                             <th>Age</th>
                             <th>Address</th>
-                            <th>Gender</th>
                             <th>Problem</th>
+                            <th>Gender</th>
                             <th>Action</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        {patients.map((patient, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{patient.patientName}</td>
+                                <td>{patient.mobileNumber}</td>
+                                <td>{patient.age}</td>
+                                <td>{patient.address}</td>
+                                <td>{patient.problem}</td>
+                                <td>{patient.gender}</td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </Table>
-            </div>
-        </>
-    )
+            )}
+        </div>
+    );
 }
 
-export default Homepage;
+export default Home;
